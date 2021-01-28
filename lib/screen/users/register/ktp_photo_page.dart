d6e148eb83567d4fd6e105b2ceaba44b676c28ff
Andika .R <andika15ramdani@gmail.com>
2021-01-28 11:55:47 +0700

    20210128 - 1154 - layout on kpt_photo_page

--- lib/screen/users/register/ktp_photo_page.dart
+++ lib/screen/users/register/ktp_photo_page.dart
@@ -1,9 +1,14 @@
 import 'dart:io';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
+import '../../../utility/style.dart';
+import '../../components/background.dart';
+import 'register_success.dart';
 
 class KtpPhotoPage extends StatefulWidget {
   @override
@@ -17,55 +22,122 @@ class _KtpPhotoPageState extends State<KtpPhotoPage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter ML Kit Text'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        width: double.infinity,
-        child: Column(
-          children: [
-            Expanded(
-              flex: 3,
-              child: imageFile == null
-                  ? Container()
-                  : Image.file(
-                      imageFile,
-                      fit: BoxFit.cover,
-                    ),
-            ),
-            Expanded(
-              child: Center(
-                child: Text(result),
-                // child: isLoading ? _buildWidgetLoading() : Text(result),
-              ),
-            ),
-          ],
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(
+            Icons.arrow_back,
+            color: Color(0xff3F414E),
+            size: 30,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        onPressed: () async {
-          var pickedFile =
-              await ImagePicker().getImage(source: ImageSource.camera);
-          if (pickedFile != null) {
-            setState(() {
-              isLoading = true;
-            });
-            imageFile = File(pickedFile.path);
-            var firebaseVisionImage = FirebaseVisionImage.fromFile(imageFile);
-            var textRecognizer = FirebaseVision.instance.textRecognizer();
-            var visionText =
-                await textRecognizer.processImage(firebaseVisionImage);
-            result = 'Result: ${visionText.text}';
-            debugPrint('result: $result');
-            textRecognizer.close();
-            setState(() {
-              isLoading = false;
-            });
-          }
-        },
+      body: Background(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          // width: double.infinity,
+          child: Column(
+            children: [
+              Text(
+                'Foto KTP',
+                style: title900Dark,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Expanded(
+                flex: 3,
+                child: imageFile == null
+                    ? Container()
+                    : Image.file(
+                        imageFile,
+                        fit: BoxFit.cover,
+                      ),
+              ),
+              Expanded(
+                child: Center(
+                  // child: Text(result),
+                  child: isLoading ? _buildWidgetLoading() : Text(result),
+                ),
+              ),
+              AutoSizeText.rich(
+                TextSpan(
+                  style: text500Grey,
+                  children: [
+                    TextSpan(
+                      text: 'Klik ',
+                    ),
+                    TextSpan(
+                      text: 'LANJUTKAN ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text:
+                          'untuk proses selanjutnya atau kembali untuk foto ulang',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+                maxLines: 2,
+                presetFontSizes: [16, 12, 8],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              FlatButton(
+                onPressed: () async {
+                  var pickedFile =
+                      await ImagePicker().getImage(source: ImageSource.camera);
+                  if (pickedFile == null) {
+                  } else {
+                    setState(
+                      () {
+                        isLoading = true;
+                      },
+                    );
+                    imageFile = File(pickedFile.path);
+                    var firebaseVisionImage =
+                        FirebaseVisionImage.fromFile(imageFile);
+                    var textRecognizer =
+                        FirebaseVision.instance.textRecognizer();
+                    var visionText =
+                        await textRecognizer.processImage(firebaseVisionImage);
+                    result = 'Result: ${visionText.text}';
+                    debugPrint('result: $result');
+                    textRecognizer.close();
+                    setState(
+                      () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RegisterSuccess(),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+                height: 60,
+                minWidth: size.width,
+                color: Theme.of(context).primaryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  'LANJUTKAN',
+                  style: buttonTextLight,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
