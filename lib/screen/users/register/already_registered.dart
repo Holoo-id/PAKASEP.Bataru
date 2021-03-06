aa6e9c40561ebd1110951106a014bd10677c183d
Andika .R <andika15ramdani@gmail.com>
2021-03-06 22:55:47 +0700

    d

--- lib/screen/users/register/already_registered.dart
+++ lib/screen/users/register/already_registered.dart
@@ -1,7 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:pakasep/screen/components/back_only_appbar.dart';
-import 'package:pakasep/screen/components/wave_background.dart';
+import 'package:pakasep/screen/components/background.dart';
 import 'package:pakasep/utility/style.dart';
 
 class AlreadyRegistered extends StatefulWidget {
@@ -14,54 +14,69 @@ class _AlreadyRegisteredState extends State<AlreadyRegistered> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: WaveBackground(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              BackOnlyAppbar(child: null),
-              Container(
-                alignment: Alignment.center,
-                height: size.height,
-                padding: EdgeInsets.all(20),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      AutoSizeText(
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: Size(size.width, 65),
+        child: BackOnlyAppbar(
+          child: null,
+        ),
+      ),
+      body: Background(
+        child: Container(
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(
+                vertical: size.height * 0.075, horizontal: size.width * 0.1),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: AutoSizeText(
                         'Anda Sudah Terdaftar!',
                         textAlign: TextAlign.center,
                         style: title600Dark,
-                        maxLines: 1,
+                        maxLines: 2,
                         presetFontSizes: [25, 20, 15],
                       ),
-                      AutoSizeText(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: AutoSizeText(
                         'Anda sudah terdaftar di aplikasi PAKASEP. Silahkan tunggu pemberitahuan lebih lanjut.',
-                        // 'Tidak perlu mendaftar lagi Silahkan masuk ke aplikasi',
                         textAlign: TextAlign.center,
                         style: subtitle600Light2,
                         presetFontSizes: [16, 15, 10],
                       ),
-                      FlatButton(
-                        onPressed: () {
-                          Navigator.of(context).pushNamed('/intro');
-                        },
-                        height: 60,
-                        minWidth: size.width,
-                        color: Theme.of(context).primaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Text(
-                          'MENGERTI',
-                          style: buttonTextLight,
-                        ),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10, top: 25),
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/intro');
+                    },
+                    height: 60,
+                    minWidth: size.width,
+                    color: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Text(
+                      'SELESAI',
+                      style: buttonTextLight,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
